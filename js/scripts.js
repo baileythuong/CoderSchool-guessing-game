3cf1e9411c6ed14619b4c04d79b1678e8b474fc0
Bailey Nguyen <baileythuong@gmail.com>
2019-10-02 17:36:20 +0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+let guesses = 10;
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+console.log("the random number is", randomNumber);
+
+let history = [];
+
+function guessTheNumber() {
+    // counting guesses
+    guesses--;
+
+    if (guesses < 0) {
+    location.reload();
+    } else if (guesses === 0) {
+    document.getElementById("remainingGuesses").innerHTML = `Try again!`
+    } else {
+    document.getElementById("remainingGuesses").innerHTML = `${guesses} guesses left...`
+    console.log(guesses, "turn(s) left");
+    }
+
+    // getting user's input
+  let userGuess = document.getElementById("value").value;
+
+  if (history.includes(userGuess)) {
+    document.getElementById("guessPrompt").innerHTML = `You've already guessed ${userGuess}`;
+    document.getElementById("value").value = "";
+    return;
+  }
+
+    // doing the guess game
+  if (userGuess > randomNumber) {
+    document.getElementById("guessPrompt").innerHTML = `${userGuess} is too high`;
+    document.getElementById("value").value = "";
+    history.push(userGuess);
+  } else if (userGuess < randomNumber) {
+    document.getElementById("guessPrompt").innerHTML = `${userGuess} is too low`;
+    document.getElementById("value").value = "";
+    history.push(userGuess);
+  } else {
+    document.getElementById("guessPrompt").innerHTML = `${userGuess} is the correct number!`;
+    document.getElementById("value").value = "";
+    history.push(userGuess);
+  }
+  // displaying history
+  document.getElementById("history").innerHTML = `You've guessed the following numbers: ${history.join(", ")}`;
+}
+
+function startOver() {
+location.reload();
+}
+
+let time = 0; // start times
+let gameTime;
+
+function gameCounter() {
+    gameTime = setInterval(function() {
+    time += 1;
+    document.getElementById("timeRemaining").innerHTML=time;
+
+    if(time === 5) {
+        stopCounter();
+    }
+    }, 1000);
+}
+
+function stopCounter() {
+    clearInterval(gameTime);
+}
+
+gameCounter();
